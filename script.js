1de26ad59c166add9e753e53aa6061221a992b79
lisieux-s <sophiazocoler@gmail.com>
2021-11-04 15:37:13 -0300

    javascript iniciado

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//teste 3
+n = prompt("Com quantas cartas você quer jogar?");
+
+//repetir prompt até entrar n válido
+if( (n < 4 || n > 14) && n % 2 !== 0) {
+    n = prompt("Com quantas cartas você quer jogar?");
+}
+
+//gerar array com n elementos
+
+let cardsArray = [];
+for(i = 0; i == n; i++){
+    cardsArray.push(i)
+}
+
+alert(cardsArray)
