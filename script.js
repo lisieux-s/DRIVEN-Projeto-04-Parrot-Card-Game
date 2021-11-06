20868aea8ef76343d4e69d881b7d124a8f21f46e
lisieux-s <sophiazocoler@gmail.com>
2021-11-06 15:48:26 -0300

    pares são embaralhados e distribuídos

--- script.js
+++ script.js
@@ -1,66 +1,40 @@
 n = prompt("Com quantas cartas você quer jogar?");
 const main = document.querySelector("main");
-let imgBackRandom = [];
 
 //repetir prompt até entrar n válido
 while( (n < 4 || n > 14) || n % 2 !== 0) {
     n = prompt("Com quantas cartas você quer jogar?");
 }
+console.log(n);
 
-//gerar baralho
-const cardsArray = [];
-for(let i = 0; i < n; i++){
-    cardsArray.push(i);
+//gerar n/2 pares
+const pairs = []
+for(let i = 1; i <= n/2; i++){    
+        pairs.push(i);
+        pairs.push(i);
 }
+console.log(pairs);
 
-//distribuir os pares de cartas aleatoriamente
-card = [];
+//embaralhar pares
+const pairsShuffled = pairs.sort(comparador);
+console.log(pairsShuffled);
+
+//inserir array embaralhada como cartas no html
+
 for(let i = 0; i < n; i++) {
-        /*if(i % 2 === 0) {
-            let cardCandidate = 0;
-            let candidateChosen = false;
-            let instances = 1;
-
-            //processo seletivo de candidato a carta:
-            //um valor para carta só pode aparecer duas vezes
-            //se o valor já existe em duas cartas, escolher outro valor
-            while(candidateChosen == false){
-                cardCandidate = Math.floor(Math.random()*8);
-                for(let j = 1; j < n; j++){
-                    if(cardCandidate = card[j]){
-                        instances++;
-                    }
-                }
-                if(instances <= 2){
-                    candidateChosen = true;
-                } else {
-                    candidateChosen = false;
-                }
-            }
-            
-            //ao vitorioso, os espólios
-            card[i] = cardCandidate;
-*/
-
             main.innerHTML +=
         `
             <article data-identifier="card" onclick="flipCard(this)">
                 <div class="back hide" data-identifier="back-face">
-                    <img src="img/${i}.gif">
+                    <img src="img/${pairsShuffled}.gif">
                 </div>
                 <div class="front show" data-identifier="front-face">
-                    <img src="img/front.png"> ${i}
+                    <img src="img/front.png"> ${pairsShuffled[i]}
                 </div>
             </article>
     
         `;
         }
-
-function randomCard(){
-    const card = Math.floor(Math.random()*8);
-    return card;
-    
-}
 
 function flipCard(card){
     card.classList.toggle(".hide");
